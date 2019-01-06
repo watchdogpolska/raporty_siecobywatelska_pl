ca3bdd9554f8c5e89addbf6d7241db124f53e7ca
Miklaj <miklaj@private.private>
2019-01-06 17:58:24 +0100

    Enable Django Debug Toolbar in docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -46,12 +46,13 @@ CACHES = {
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 INSTALLED_APPS += ['debug_toolbar', ]
 
-INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
         'debug_toolbar.panels.redirects.RedirectsPanel',
     ],
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True,
     'SHOW_TEMPLATE_CONTEXT': True,
 }
 
